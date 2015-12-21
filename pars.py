1a1c53c54d664a2852cde36af9b7fe9603a8338a
MartijnAB <g.m.a.b@st.hanze.nl>
2015-12-21 15:28:49 +0100

    pars

--- pars.py
+++ pars.py
@@ -10,7 +10,10 @@ class Pars:
         parser.add_argument('-alt', type=str, help='als vcf or out guacemole')
         parser.add_argument('-ref', type=str, help='ref vcf or out guacemole')
         args = parser.parse_args()
-
+        if args.alt == None or args.ref == None:
+            print("ja")
+        else:
+            print("nee")
 
 
 def main():
