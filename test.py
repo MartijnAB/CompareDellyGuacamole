cc0f5fa2eda072c870304cc9436b1710383b0a47
MartijnAB <g.m.a.b@st.hanze.nl>
2015-12-22 11:15:35 +0100

    compare test vcf

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
- def positive_or_negative(self, interval_vcf_start, interval_vcf_stop, interval_guacemole_start,
-                             interval_guacemole_stop):
+ def positive_or_negative(self, interval_vcft_start, interval_vcft_stop, interval_vcf_start, interval_vcf_stop):
+        interval_vcft = interval_vcft_stop - interval_vcft_start
         interval_vcf = interval_vcf_stop - interval_vcf_start
-        interval_guacemole = interval_guacemole_stop - interval_guacemole_start
-        if interval_vcf >= interval_guacemole:
-            if (interval_vcf_start <= interval_guacemole_start <= interval_vcf_stop) or (
-                    interval_vcf_start <= interval_guacemole_stop <= interval_vcf_stop):
+        if interval_vcft >= interval_vcf:
+            if (interval_vcft_start <= interval_vcf_start <= interval_vcft_stop) or (
+                    interval_vcft_start <= interval_vcf_stop <= interval_vcft_stop):
                 return True
             else:
                 return False
         else:
-            if (interval_guacemole_start < interval_vcf_start < interval_guacemole_stop) or (
-                    interval_guacemole_start < interval_vcf_stop < interval_guacemole_stop):
+            if (interval_vcf_start < interval_vcft_start < interval_vcf_stop) or (
+                    interval_vcf_start < interval_vcft_stop < interval_vcf_stop):
                 return True
             else:
                 return False
